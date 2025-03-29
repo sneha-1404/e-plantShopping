b47dfa502116ad961d44ac6e48ea3799e05fbc18
sneha-1404 <sneha.22210052@viit.ac.in>
2025-03-29 02:29:50 -0400

    change

--- vite.config.js
+++ vite.config.js
@@ -3,8 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/shoppingreact",
-  base: "/e-plantShopping
-",
+  base: "/e-plantShopping", // Ensure this string is properly closed
   plugins: [react()],
 })
